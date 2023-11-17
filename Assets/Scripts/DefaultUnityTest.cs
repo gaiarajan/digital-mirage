abe1aafa4efb85cfe944a0d60a74ba01a2294598
Buckblack2 <bmblack@andrew.cmu.edu>
2023-11-17 18:34:24 -0500

    BetaTestSceneCompleted

---
+++ Assets/Scripts/DefaultUnityTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultUnityTest : MonoBehaviour
+{
+
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
