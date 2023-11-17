a687e76570049bce84e2a5a4ac0c6e8964a89159
Buckblack2 <bmblack@andrew.cmu.edu>
2023-11-16 21:16:55 -0500

    AllBetaSoundTestObjectsCreated

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,19 +7,29 @@ using FMODUnity;
 public class SoundManager : MonoBehaviour
 {
     // Game Objects
-    /*public GameObject fountain;
-    public GameObject guitar;
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+    public GameObject object4;
+    public GameObject object5;
     
     
     // Event Instances
-    private FMOD.Studio.EventInstance fountainInstance;
-    private FMOD.Studio.EventInstance guitarInstance;
+    private FMOD.Studio.EventInstance object1Instance;
+    private FMOD.Studio.EventInstance object2Instance;
+    private FMOD.Studio.EventInstance object3Instance;
+    private FMOD.Studio.EventInstance object4Instance;
+    private FMOD.Studio.EventInstance object5Instance;
     
     // Start is called before the first frame update
     void Start()
     {
-        fountainInstance = AmbientStart(fountain, "event:/Fountain");
-        guitarInstance = AmbientStart(guitar, "event:/Guitar");
+        object1Instance = SoundStart(object1, "event:/Guitar");
+        object2Instance = SoundStart(object2, "event:/Guitar");
+        object3Instance = SoundStart(object3, "event:/Guitar");
+        object4Instance = SoundStart(object4, "event:/Guitar");
+        object5Instance = SoundStart(object5, "event:/Guitar");
+        
     }
     
     // Update is called once per frame
@@ -28,7 +38,7 @@ public class SoundManager : MonoBehaviour
         
     }
 
-    public FMOD.Studio.EventInstance AmbientStart(GameObject obj, string path)
+    public FMOD.Studio.EventInstance SoundStart(GameObject obj, string path)
     {
         if (obj != null)
         {
@@ -39,6 +49,6 @@ public class SoundManager : MonoBehaviour
             return eventInstance;
         }
         return new EventInstance();
-    }*/
+    }
     
 }
