4a294b8877f3107b6b4581597ab44675a46e6ef5
OlegP-andrew <oplisov@andrew.cmu.edu>
2023-11-20 03:53:10 -0500

    Added VR device simulator & completed test functionality

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,54 +1,131 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using FMOD;
-using FMOD.Studio;
-using FMODUnity;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.IO;
 public class SoundManager : MonoBehaviour
 {
     // Game Objects
-    public GameObject object1;
-    public GameObject object2;
-    public GameObject object3;
-    public GameObject object4;
-    public GameObject object5;
-    
-    
-    // Event Instances
-    private FMOD.Studio.EventInstance object1Instance;
-    private FMOD.Studio.EventInstance object2Instance;
-    private FMOD.Studio.EventInstance object3Instance;
-    private FMOD.Studio.EventInstance object4Instance;
-    private FMOD.Studio.EventInstance object5Instance;
+    public GameObject[] sounds;
+    public GameObject[] visual1;
+    public GameObject[] visual2;
+    public InputActionReference trigger;
+    public InputActionReference next;
+    private Stack<GameObject> actives = new Stack<GameObject>();
+    public XRRayInteractor GuessRay;
+
+    private bool test = false;
+    private int cycle = 0;
+    private int attempt = 0;
+    public int attemptNumber = 3;
+    private int n;
+    private string testRes = "";
     
     // Start is called before the first frame update
     void Start()
     {
-        object1Instance = SoundStart(object1, "event:/Guitar");
-        object2Instance = SoundStart(object2, "event:/Guitar");
-        object3Instance = SoundStart(object3, "event:/Guitar");
-        object4Instance = SoundStart(object4, "event:/Guitar");
-        object5Instance = SoundStart(object5, "event:/Guitar");
-        
+        n = sounds.Length;
+        Debug.Log(Application.persistentDataPath + "/test.txt");
     }
     
     // Update is called once per frame
     void Update() 
     {
-        
+
     }
 
-    public FMOD.Studio.EventInstance SoundStart(GameObject obj, string path)
+    void OnEnable()
     {
-        if (obj != null)
+        trigger.action.Enable();
+        trigger.action.performed += OnGuess;
+        next.action.Enable();
+        next.action.performed += OnNext;
+    }
+
+    void OnGuess(InputAction.CallbackContext context)
+    {
+        if (test)
         {
-            Transform objTransform = obj.transform;
-            FMOD.Studio.EventInstance eventInstance = FMODUnity.RuntimeManager.CreateInstance(path);
-            FMODUnity.RuntimeManager.AttachInstanceToGameObject(eventInstance, objTransform);
-            eventInstance.start();
-            return eventInstance;
+            testRes += "Attempt #" + attempt.ToString() + ": ";
+            GetGuess();
+            testRes += "\n";
+            ++attempt;
+            if (attempt == attemptNumber)
+            {
+                EndCurrent();
+                return;
+            }
         }
-        return new EventInstance();
+    }
+
+    void OnNext(InputAction.CallbackContext context)
+    {
+        if (!test){
+            StartNext();
+        }
+    }
+
+    private RaycastHit hit;
+    public GameObject origin;
+    void GetGuess()
+    { 
+        GuessRay.TryGetCurrent3DRaycastHit(out hit);
+        testRes += "From: " + origin.transform.position.ToString() + "\nTo: " + hit.point.ToString();
+        testRes += "\n";
     }
     
+    void EndCurrent()
+    {
+        test = false;
+        while(actives.Count > 0) {
+            Deactivate();
+        }
+        ++cycle;
+        testRes += "End of test\n========================\n";
+        if (cycle == n * 3) {
+            string path = Application.persistentDataPath + "/test.txt";
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine("Test");
+            writer.WriteLine(testRes);
+            writer.Close();
+            StreamReader reader = new StreamReader(path);
+            //Print the text from the file
+            Debug.Log(reader.ReadToEnd());
+            reader.Close();
+        }
+    }
+
+    void StartNext()
+    {
+        testRes += "Test #" + cycle.ToString() + "\n------------------------\n";
+        test = true;
+        int iter = cycle % n;
+        attempt = 0;
+        Activate(sounds[iter]);
+        switch(cycle / n)
+        {
+            case 1:
+                Activate(visual1[iter]);
+                break;
+            case 2:
+                Activate(visual2[iter]);
+                break;
+        }
+    }
+
+    void Activate(GameObject obj)
+    {
+        obj.SetActive(true);
+        actives.Push(obj);
+    }
+
+    //Deactivate top of the actives stack
+    void Deactivate()
+    {
+        GameObject obj = actives.Pop();
+        obj.SetActive(false);
+    }
+
+
 }
